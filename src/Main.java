4f995f9d84b83edee061966d1b0f8309143fae14
Ritwik <deshrutu@gmail.com>
2022-05-03 18:00:26 +0530

    UC2 - Employee Daily Wage

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,25 @@ public class Main {
     public static void main(String[] args) {
 
         int IS_FULL_TIME = 1;
+        int WAGE_PER_HR = 20;
+
+        int empHrs;
+        int empWage;
         double check = Math.floor(Math.random() * 10) % 2;
 
         System.out.println("---------------------------------------------");
         System.out.println("Welcome to Employee Wage Computation Problem.");
+        System.out.println("---------------------------------------------");
 
         if (check == IS_FULL_TIME){
             System.out.println("Employee is Present.");
+            empHrs = 8;
         }else{
             System.out.println("Employee is Absent.");
+            empHrs = 0;
         }
+
+        empWage = empHrs * WAGE_PER_HR;
+        System.out.println("Employee Wage: " + empWage);
     }
 }
