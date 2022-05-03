5c6b212c62e90dd939ffb3b8bc04c5ae3253ab42
Ritwik <deshrutu@gmail.com>
2022-05-03 22:23:26 +0530

    UC6 - Monthly Wages on Condition

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,23 @@ public class Main {
         int IS_PART_TIME = 2;
         int WAGE_PER_HR = 20;
         int TOTAL_WORKING_DAYS = 20;
+        int MAX_WORKING_HRS = 100;
 
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        System.out.println("---------------------------------------------");
         System.out.println("Welcome to Employee Wage Computation Problem.");
-        System.out.println("---------------------------------------------");
 
-        for(int i = 0; i < TOTAL_WORKING_DAYS; i++ ){
+        while(totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < TOTAL_WORKING_DAYS) {
+            totalWorkingDays++;
+
+            System.out.println("Day: " + totalWorkingDays);
+
             double check = Math.floor(Math.random() * 10) % 3;
+
             switch((int)check) {
                 case 1:
                     System.out.println("Employee is Present.");
@@ -30,9 +36,12 @@ public class Main {
                     empHrs = 0;
                     break;
             }
-            empWage = empHrs * WAGE_PER_HR;
-            totalEmpWage += empWage;
-            System.out.println("Employee Wage: " + empWage);
+                empWage = empHrs * WAGE_PER_HR;
+                totalEmpWage += empWage;
+
+                System.out.println("Employee Wage: " + empWage);
+                System.out.println("Hours Worked: " + empHrs);
+                System.out.println("-------------------------");
         }
         System.out.println("Total Employee Wage: " + totalEmpWage);
     }
