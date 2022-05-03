7153a6be65a6a18e081e0f5bf5082e322c0dec63
Ritwik <deshrutu@gmail.com>
2022-05-03 18:05:00 +0530

    UC3 - Part time Employee and Wage

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int WAGE_PER_HR = 20;
 
         int empHrs;
         int empWage;
-        double check = Math.floor(Math.random() * 10) % 2;
+        double check = Math.floor(Math.random() * 10) % 3;
 
         System.out.println("---------------------------------------------");
         System.out.println("Welcome to Employee Wage Computation Problem.");
@@ -15,7 +16,10 @@ public class Main {
         if (check == IS_FULL_TIME){
             System.out.println("Employee is Present.");
             empHrs = 8;
-        }else{
+        } else if (check == IS_PART_TIME) {
+            System.out.println("Employee is Present Part-time.");
+            empHrs = 4;
+        } else{
             System.out.println("Employee is Absent.");
             empHrs = 0;
         }
