9f8b49e143798d1743eb081d8108e0c418471b62
Ritwik <deshrutu@gmail.com>
2022-05-03 13:06:56 +0530

    UC1 - Attendance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        int IS_FULL_TIME = 1;
+        double check = Math.floor(Math.random() * 10) % 2;
+
         System.out.println("---------------------------------------------");
         System.out.println("Welcome to Employee Wage Computation Problem.");
+
+        if (check == IS_FULL_TIME){
+            System.out.println("Employee is Present.");
+        }else{
+            System.out.println("Employee is Absent.");
+        }
     }
 }
